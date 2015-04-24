34f1eef946c52f24e2e35b5a185951e085f7ecea
Henry He <henry@Henrys-MacBook-Air.local>
2015-04-24 10:56:50 -0700

    Added in logic for labelling user passed in markup

--- src/js/navitron.js
+++ src/js/navitron.js
@@ -45,6 +45,8 @@
 
             this._validateOptions();
 
+            this._labelTree();
+
             this._build();
 
             this._addAccessibility();
@@ -56,6 +58,32 @@
 
         destroy: function() {
             this.$navitron.removeData(this.name);
+        },
+
+        _labelTree: function() {
+            var selector = '> li > ul';
+            this.$navitron.attr('data-level', '0');
+
+            while (true) {
+                var $children = this.$navitron.find(selector);
+
+                if ($children.length) {
+
+                    $children.each(function (index, item) {
+                        var $child = $(item);
+                        // Grabbing ul parent
+                        var $parent = $child.parents('ul').first();
+
+                        if ($parent.length) {
+                            $child.attr('data-level', $parent.attr('data-level') + '.' + index);
+                        }
+                    });
+
+                    selector += selector;
+                } else {
+                    break;
+                }
+            }
         },
 
         _build: function() {
@@ -332,7 +360,6 @@
         },
 
         _getTargetPane: function(pane) {
-            debugger;
             return this.$navitron.find(selectors.PANE + '[data-level="' + pane + '"]');
         },
 
