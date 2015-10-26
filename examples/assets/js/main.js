74ab1eec59fd6d7874c1b0242924fccd3c98a428
Mike Lee <mlee@mobify.com>
2015-10-26 10:04:31 -0700

    Testing form markups

--- examples/assets/js/main.js
+++ examples/assets/js/main.js
@@ -5,9 +5,17 @@ require(['config'], function() {
         'navitron'
     ],
     function($) {
-        $('#myNavitron').navitron({
+        var $navitron = $('#myNavitron');
+
+        $navitron.navitron({
             structure: false,
             shiftAmount: 100
+        });
+
+        var $panes = $navitron.find('.navitron__pane');
+
+        $('body').on('click', '.js-back-to-first', function() {
+            $navitron.navitron('showPane', $panes.first());
         });
     });
 });
